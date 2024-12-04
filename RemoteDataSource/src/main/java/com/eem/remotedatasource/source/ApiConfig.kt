aabc0b3693363f421da56b5a325a6c3644fd8f15
Elias Eguizabal <emendoza@applaudostudios.com>
2024-12-03 20:35:22 -0600

    :arrow_up: Add Retrofit dependency

---
+++ RemoteDataSource/src/main/java/com/eem/remotedatasource/source/ApiConfig.kt
@@ -0,0 +1,8 @@
+package com.eem.remotedatasource.source
+
+object ApiConfig {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_READ_TOKEN =
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjE1Y2Y2ZTExNzA1ODExMjFkMTZhNmM3ZDdlNGM5ZSIsIm5iZiI6MTYxMjE5NzAyNS45NzMsInN1YiI6IjYwMTgyY2ExODdhMjdhMDA0MGU0OTVjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gBcW0wu_w5-MAcIz8LbWtzwTehIuAFz_V62D-zphRNE"
+    const val API_KEY = "2615cf6e1170581121d16a6c7d7e4c9e"
+}
